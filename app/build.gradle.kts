598158c957347644e16f54722ceb56bc0175e55b
Vivek Sagar <airborne.reaver@gmail.com>
2025-09-28 14:10:33 +0530

    Revert to alias plugins syntax for CI compatibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
 }
 
 android {
