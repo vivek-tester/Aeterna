c36821b536b819e10ec7538cede7776e1e5ee32b
Vivek Sagar <airborne.reaver@gmail.com>
2025-09-28 14:05:50 +0530

    Fix build.gradle.kts plugins to use direct IDs instead of alias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
 }
 
 android {
